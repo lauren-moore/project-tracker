8293e9cc99cf21985e7a976f7d6cff522a389d50
Lauren <laurencaroleen@gmail.com>
2021-12-24 05:11:11 +0000

    Completed Project Tracker

---
+++ project-tracker.sql
@@ -0,0 +1,165 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.4 (Ubuntu 13.4-4.pgdg20.04+1)
+-- Dumped by pg_dump version 13.4 (Ubuntu 13.4-4.pgdg20.04+1)
+
+SET statement_timeout = 0; 
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: hackbright
+--
+
+CREATE TABLE public.projects (
+    id integer NOT NULL,
+    title character varying(30),
+    description text,
+    max_grade integer
+);
+
+
+ALTER TABLE public.projects OWNER TO hackbright;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: hackbright
+--
+
+CREATE SEQUENCE public.projects_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.projects_id_seq OWNER TO hackbright;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hackbright
+--
+
+ALTER SEQUENCE public.projects_id_seq OWNED BY public.projects.id;
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: hackbright
+--
+
+CREATE TABLE public.students (
+    id integer NOT NULL,
+    first_name character varying(30),
+    last_name character varying(30),
+    github character varying(30)
+);
+
+
+ALTER TABLE public.students OWNER TO hackbright;
+
+--
+-- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: hackbright
+--
+
+CREATE SEQUENCE public.students_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.students_id_seq OWNER TO hackbright;
+
+--
+-- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: hackbright
+--
+
+ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
+
+
+--
+-- Name: projects id; Type: DEFAULT; Schema: public; Owner: hackbright
+--
+
+ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.projects_id_seq'::regclass);
+
+
+--
+-- Name: students id; Type: DEFAULT; Schema: public; Owner: hackbright
+--
+
+ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: hackbright
+--
+
+COPY public.projects (id, title, description, max_grade) FROM stdin;
+1	Markov	Tweets generated from Markov chains	50
+2	Blockly	Programmatic logic puzzle game	100
+3	Shopping Site	Create web app to sell melons	99
+4	Shark Words	Create hangman game but with sharks	60
+5	Project Tracker	Create tracker to track projects	75
+\.
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: hackbright
+--
+
+COPY public.students (id, first_name, last_name, github) FROM stdin;
+1	Jane	Hacker	jhacks
+2	Sarah	Developer	sdevelops
+\.
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hackbright
+--
+
+SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
+
+
+--
+-- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: hackbright
+--
+
+SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+
+
+--
+-- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: hackbright
+--
+
+ALTER TABLE ONLY public.projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: hackbright
+--
+
+ALTER TABLE ONLY public.students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
